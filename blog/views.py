dbd5e721be308d36eba3aa0170793302fe6ca186
ppogulailo <pavel.pogulailo@gmail.com>
2024-03-10 11:30:33 +0200

    init

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def starting_page(req):
+    return render(req,"blog/index.html")
+
+def posts(req):
+    pass
+
+def post_detail(req):
+    pass
