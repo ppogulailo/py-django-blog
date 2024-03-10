d8f4a7bcb79b9fb4cc90c0b242f0e33de1eca51f
ppogulailo <pavel.pogulailo@gmail.com>
2024-03-10 12:56:23 +0200

    S-5 templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("posts/",views.posts, name='posts-page'),
+    path("posts/", views.posts, name='posts-page'),
     path("posts/<slug:slug>", views.post_detail, name='posts-detail-page')   # /posts/id
 ]
